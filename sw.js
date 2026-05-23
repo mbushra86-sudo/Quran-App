4525945c97cd33e03e8b402f7e2da62b7aa8b15f
mbushra86-sudo <m.bushra86@gmail.com>
2026-05-23 12:29:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+    .then(() => self.registration.unregister())
+  );
+  self.clients.claim();
+});
